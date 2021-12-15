93ec2e05a796aa333e82d8c9afc7dc4956fa69d0
SaminaAfrinDhola <faridpurweb@gmail.com>
2021-12-15 23:24:31 +0600

    this is my test

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>sharriweb</title>
+	<link rel="stylesheet" type="text/css" href="stlye.css">
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<section class="part1">
+		<div class="row" >
+			<div class="col col-sm-4" >
+				<img class="logo" src="img/logo.png">
+
+			</div>
+			<div class="col col-sm-6" style="float: right;">
+				<input class="form-control" type="text" name="">
+			</div>
+			
+		</div>
+
+
+
+
+
+
+	<header><div class=""></div> 
+		
+
+		<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About Us</a></li>
+				<li><a href="#">Contact</a></li>
+				<li><a href="#">Discount</a></li>
+				<li><a href="#">Spectial </a></li>
+
+			</ul></header>
+</section>
+
+
+
+
+
+
+<script ></script>
+</body>
+</html>
